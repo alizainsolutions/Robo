ed0437b69841429a80e6e26d5ba6c6e03c10cec9
alizainsolutions <alizainsolutions@gmail.com>
2025-06-14 22:04:34 +0500

    A simple change in Toast message

--- app/src/main/java/com/alizainsolutions/robo/MainActivity.java
+++ app/src/main/java/com/alizainsolutions/robo/MainActivity.java
@@ -371,7 +371,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Microphone permission granted", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Microphone permission granted!", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(this, "Microphone permission denied", Toast.LENGTH_SHORT).show();
             }
